20f49226f62290f6dbc31c088ba9e134744cfc83
Tiller, Nick <Nick.Tiller@nike.com>
2016-02-16 19:16:51 -0800

    Initial commit

---
+++ app/src/main/java/edu/lclark/networkapplication/NetworkAsyncTask.java
@@ -0,0 +1,78 @@
+package edu.lclark.networkapplication;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by ntille on 2/16/16.
+ */
+public class NetworkAsyncTask extends AsyncTask<String, Integer, JSONArray> {
+
+
+    public static final String TAG = NetworkAsyncTask.class.getSimpleName();
+
+
+
+    @Override
+    protected JSONArray doInBackground(String... params) {
+
+        StringBuilder responseBuilder = new StringBuilder();
+        JSONArray jsonArray = null;
+        if (params.length == 0) {
+            return null;
+        }
+
+        String userId = params[0];
+
+        try {
+            URL url = new URL("https://api.github.com/users/" + userId + "/subscriptions");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+
+            InputStreamReader inputStream = new InputStreamReader(connection.getInputStream());
+            BufferedReader reader = new BufferedReader(inputStream);
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                responseBuilder.append(line);
+            }
+
+            jsonArray = new JSONArray(responseBuilder.toString());
+
+        } catch (IOException | JSONException e) {
+            Log.e(TAG, e.getLocalizedMessage());
+        }
+
+        return jsonArray;
+    }
+
+    @Override
+    protected void onPostExecute(JSONArray jsonArray) {
+        super.onPostExecute(jsonArray);
+        if (jsonArray == null) {
+            Log.e(TAG, "Resulting JSON is null");
+        } else {
+            for (int i = 0; i < jsonArray.length(); i++) {
+                try {
+                    JSONObject subscription = jsonArray.getJSONObject(i);
+                    JSONObject owner = subscription.getJSONObject("owner");
+
+                    Log.d(TAG, owner.getString("login") + " owns " + subscription.getString("name"));
+                } catch (JSONException e) {
+                    Log.e(TAG, e.getLocalizedMessage());
+                }
+            }
+            Log.d(TAG, jsonArray.toString());
+        }
+    }
+}
